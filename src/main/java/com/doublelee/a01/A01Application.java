4e8ed84a2df8d5422167192ffc089c1afa66ebd2
LeeKC-covi <kclee@covision.co.kr>
2022-01-07 17:45:55 +0900

    first init

---
+++ src/main/java/com/doublelee/a01/A01Application.java
@@ -0,0 +1,13 @@
+package com.doublelee.a01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A01Application.class, args);
+	}
+
+}
